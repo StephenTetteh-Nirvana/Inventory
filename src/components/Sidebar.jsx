680879830e3c72ba25465aab3407c9cf523648de
Nirvana <stephenarthur2707@gmail.com>
2024-04-03 01:47:07 -0700

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,51 @@
-import { Home } from "lucide-react"
+import { LayoutDashboard,ShoppingBasket,Warehouse,UsersRound,FileText,MessageCircleWarning,ArrowLeft,ArrowRight } from "lucide-react"
+import { useState } from "react"
+import "../css/Sidebar.css"
 
 const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-       <Home />
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+    <div className="toggle-btn-container">
+        <button className="toggle-btn" onClick={toggleSidebar}>
+        {isOpen ? <ArrowLeft size={25} /> : <ArrowRight size={25} />}
+        </button>
     </div>
-  )
+     
+      <ul>
+        <li>
+          <LayoutDashboard />
+          {isOpen && <p>Dashboard</p>}
+        </li>
+        
+        <li>
+          <Warehouse />
+          {isOpen && <p>Warehouse</p>}
+        </li>
+        <li>
+          <ShoppingBasket />
+          {isOpen && <p>Products</p>}
+        </li>
+        <li>
+          <FileText />
+          {isOpen && <p>Orders</p>}
+        </li>
+        <li>
+          <UsersRound />
+          {isOpen && <p>Users</p>}
+        </li>
+        <li>
+          <MessageCircleWarning />
+          {isOpen && <p>Report</p>}
+        </li>
+      </ul>
+    </div>
+     )
 }
 
 export default Sidebar
