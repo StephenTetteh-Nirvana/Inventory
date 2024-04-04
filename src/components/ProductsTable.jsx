7d118825f63ac2f7b53ead3f053077dc79199cc2
Nirvana <stephenarthur2707@gmail.com>
2024-04-03 23:39:59 -0700

    update

--- src/components/ProductsTable.jsx
+++ src/components/ProductsTable.jsx
@@ -1,9 +1,19 @@
-
+import { Plus,Search } from "lucide-react"
+import "../css/ProductsTable.css"
 
 const ProductsTable = () => {
   return (
-    <div>
-        <h1>Products</h1>
+    <div className="products-table">
+         <div className="search-and-add-section">
+            <div className="products-search-section">
+            <span><Search /></span><input type="text" placeholder="Search..."/>
+            </div>
+
+            <div className="add-product">
+                <button>Add New Product</button><span><Plus style={{color:"white"}}/></span>
+            </div>
+         </div>
+        <h1>Products</h1> 
     </div>
   )
 }
